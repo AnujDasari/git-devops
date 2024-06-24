306a221b567648f9efe5ca8485a1365676c9b6af
AnujDasari <anuj.dsr@gmail.com>
2024-06-24 22:51:29 +0530

    Added a new chunk of code

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -5,10 +5,17 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		HelloWorld obj = new HelloWorld();
 		obj.welcomeMessage();
+
+		int sum = obj.addNumbers(5, 4);
+		System.out.println("The sum of the 2 numbers is: " + sum);
 	}
-	
+
 	public void welcomeMessage() {
 		System.out.println("Hi All... Welcome to Devops certification program!");
 	}
 
+	public int addNumbers(int a, int b) {
+		int sum = a + b;
+		return sum;
+	}
 }
